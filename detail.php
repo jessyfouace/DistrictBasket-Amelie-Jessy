20be4defa22e1ebf96b8ea68402c5239a8202ccf
Jessy Fouace <jessy.fouace62@gmail.com>
2018-09-17 13:30:55 +0200

    Initialize to php + mysql

--- detail.php
+++ detail.php
@@ -1,44 +1,45 @@
+<?php require("config.php"); ?>
 <?php
-include("data.php");
+// Take the table articles and only take the id same as product (url)
+$shoestable = $bdd->prepare('SELECT * FROM articles WHERE id = :idverif');
+$shoestable->execute(array(
+  'idverif' => $_GET['product']
+));
+$shoestable = $shoestable->fetchAll();
+// Take the table image and only take the article_id (same id as shoestable id) compare with product (url)
+$shoesimg = $bdd->prepare('SELECT * FROM image WHERE article_id = :idverifimg');
+$shoesimg->execute(array(
+  'idverifimg' => $_GET['product']
+));
+$shoesimg = $shoesimg->fetchAll();
 ?>
 <!doctype html>
 <html class="no-js" lang="fr">
 
 <head>
-  <title><?php echo $shoestable[$_GET['product']]['nameshoes']; ?> | DistricBasket</title>
-  <meta name="description" content="<?php echo $shoestable[$_GET['product']]['descshoes']; ?>">
+  <title><?php echo $shoestable[0]['article_name']; ?> | DistricBasket</title>
+  <meta name="description" content="<?php echo $shoestable[0]['article_desc']; ?>">
 
   <?php
   include("header.php");
   ?>
+  <?php
 
-  <div class="container containerfordetail pt-5">
-    <div class="row pt-5">
-      <div class="forimg col-10 col-sm-6 col-md-6 col-lg-6 mx-auto">
-        <img class="sizeimg" alt="Image du produits détailler" <?php echo $shoestable[$_GET['product']]['srcimg']; ?>>
-      </div>
-      <div class="desc pl-3 col-10 col-sm-6 col-md-6 col-lg-6 mx-auto">
-        <h1><?php echo $shoestable[$_GET['product']]['nameshoes']; ?></h1>
-        <p><?php echo $shoestable[$_GET['product']]['descshoes'] ?></p>
-        <p><?php echo "Prix: " . $shoestable[$_GET['product']]['priceshoes']; ?></p>
-        <ul class="list-unstyled listfordetail">
-          <?php $tablesize = $shoestable[$_GET['product']]['sizeshoes']?>
-          <?php foreach ($shoestable[$_GET['product']]['sizeshoes'] as $key => $value) {
-            echo "<li class='ml-1'>$tablesize[$key]</li>";
-            }
-          ?>
-        </ul>
-        <ul class="list-unstyled listforcolor">
-          <?php $tablecolor = $shoestable[$_GET['product']]['colorshoes']?>
-          <?php foreach ($shoestable[$_GET['product']]['colorshoes'] as $key => $value) {
-            echo "<li class='ml-1' style='background-color: $tablecolor[$key]'></li>";
-            }
-          ?>
-        </ul>
-        <p><?php echo "Dispo: " . $shoestable[$_GET['product']]['disposhoes']; ?></p>
-      </div>
-    </div>
-  </div>
+      echo "<div class='container containerfordetail pt-5'>
+          <div class='row pt-5'>
+              <div class='forimg col-10 col-sm-6 col-md-6 col-lg-6 mx-auto'>
+                <img class='sizeimg' alt='Image du produits détailler' src='" . $shoesimg[0]['url_img'] . "'>
+              </div>
+              <div class='desc pl-3 col-10 col-sm-6 col-md-6 col-lg-6 mx-auto'>
+                <h1>" . $shoestable[0]['article_name'] . "</h1>
+                <p>" . $shoestable[0]['article_desc'] . "</p>
+                <p>Prix: " . $shoestable[0]['article_price'] . "</p>
+                <p>Dispo: " . $shoestable[0]['disposhoes'] . "</p>
+              </div>
+          </div>
+        </div>";
+
+?>
 
   <?php include("footer.php"); ?>
   <?php include('script.php') ?>
